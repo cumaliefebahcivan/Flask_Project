5822a6b904ef9e9e85ec2016874bc605ccb75dab
Cumali Bahcivan <cumalibah126@gmail.com>
2019-05-31 10:24:14 +0300

    Web sayfasıyla Led yakılma komutu eklendi.

--- flask_example.py
+++ flask_example.py
@@ -1,23 +1,46 @@
 from flask import Flask, render_template ,request
-
+import RPi.GPIO as GPIO
 app = Flask(__name__)
 
-sayi = 0
+led_1 = "Led yaniyor."
+led_0 = "Led sonmus durumda."
+
+anahtar = 0
+def ledYak(pin):
+    GPIO.output(pin,GPIO.HIGH)
+    return
+
+def ledSondur(pin):
+    GPIO.output(pin, GPIO.LOW)
+    return
+
+GPIO.setmode(GPIO.BOARD)
+
+GPIO.setup(31, GPIO.OUT)
 
 @app.route("/")
 def hello():
     print("sanirim biri bizim sayfaya girdi baskanim")
-    return render_template('index.html', x = sayi)
+    anahatar = 0
+    return render_template('index.html', x = led_0)
 
 @app.route("/ledYak")
 def led_yak():
     print("led yak diye bir sayfa istegi atti efendim. ")
-    return render_template('index.html', x = 1 )
+    anahatar = 1
+    return render_template('index.html', x = led_1 )
 
 @app.route("/ledSondur")
 def led_sondur():
     print("led yak diye bir sayfa istegi atti efendim. ")
-    return render_template('index.html', x = 0 )
+    anahatar = 0
+    return render_template('index.html', x = led_0)
+
+if anahtar == 0:
+    ledSondur(31)
+elif anahtar == 1:
+    ledSondur(31)
+
 
 
 if __name__ == '__main__':
